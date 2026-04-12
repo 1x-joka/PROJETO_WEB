6d62254fdfe0f0b5889248e1e3ccb6143f0f49f6
Joaquim <joaquimlima0612@gmail.com>
2026-04-12 18:52:58 -0300

    Resolução do Exercício

--- Projeto Controle de Gastos Pessoais/script.js
+++ Projeto Controle de Gastos Pessoais/script.js
@@ -3,24 +3,101 @@ let gastos = []
 const addGasto = () => {
     let descricao = document.getElementById("desc").value
     let valor = document.getElementById("valor").value
-    if (descricao === "") {
+    let categoria = document.getElementById("categoria").value
+
+    if (descricao === "" || valor === "" || categoria === "") {
         alert('Descreva seu gasto!')
         return
     }
-    gastos.push({descricao, valor})
+    gastos.push({
+        descricao: descricao, // Adicionando na lista "gastos" o que o usuário digita, não um valor definido
+        valor: Number(valor),
+        categoria: categoria
+    })
+
+    salvar()
+    listarGasto()
+    totalGasto()
+
+    // Limpando os campos
+    document.getElementById("desc").value = ""
+    document.getElementById("valor").value = ""
+    document.getElementById("categoria").value = ""
 }
 
 const listarGasto = () => {
+    let lista = document.getElementById("lista")
+    lista.innerHTML = ""
 
+    gastos.forEach(function(gasto,index) { // index = posição do objeto no array gastos
+        let item = document.createElement("li")
+
+        item.innerHTML = gasto.descricao + " - R$" + gasto.valor.toFixed(2) + " - " + gasto.categoria + `<button onclick="removerGasto(${index})">Remover</button>` // chama a função removerGasto passando a posição (index) daquele item
+
+        lista.appendChild(item) // Adicionando o item na lista
+    })
 }
 
-const removerGasto = () => {
-}
+const removerGasto = (index) => {
+    gastos.splice(index, 1) // removendo um item (definido pelo index/sua posição) do array
 
-const filtrarGasto = () => {
-
+    salvar()
+    listarGasto()
+    totalGasto()
 }
 
 const totalGasto = () => {
-    let total = document.getElementById("total")
+    let total = 0
+
+    gastos.forEach(function(gasto){
+        total = total + gasto.valor
+    })
+
+    document.getElementById("total").innerText = "R$" + total
+}
+
+const salvar = () => {
+    localStorage.setItem("gastos", JSON.stringify(gastos)) // salva os gastos no navegador
+}
+
+const filtrarGasto = () => {
+    let filtro = document.getElementById("filtro").value
+    let lista = document.getElementById("lista")
+    lista.innerHTML = ""
+
+    gastos.forEach(function(gasto, index){
+        switch (filtro) {
+            case "alimentacao":
+                if (gasto.categoria === "Alimentação"){
+                    criarItem(gasto, index)
+                }
+                break
+            
+            case 'transporte':
+                if (gasto.categoria === "Transporte"){
+                    criarItem(gasto, index)
+                }
+                break
+
+            case 'lazer':
+                if (gasto.categoria === "Lazer"){
+                    criarItem(gasto, index)
+                }
+                break
+
+            // caso não for nenhum dos casos acima (ou seja, TODOS no select), mostra todos os gastos 
+            default:
+                criarItem(gasto, index)
+        }
+    })
+}
+
+const criarItem = (gasto, index) => {
+    let lista = document.getElementById("lista")
+    let item = document.createElement("li")
+
+    item.innerHTML = gasto.descricao + " -R$" + gasto.valor.toFixed(2) + " - " + gasto.categoria +
+    ` <button onclick="removerGasto(${index})">Remover</button>`
+
+    lista.appendChild(item)
 }
